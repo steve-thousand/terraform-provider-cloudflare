f01020e0bbb1ef778a9bdb40b2531f031860b681
steve-thousand <sconrad@cloudflare.com>
2025-08-18 11:15:22 -0500

    an acceptance test to recreate policy order bug

---
+++ internal/services/account_token/testdata/account_token-policy-order.tf
@@ -0,0 +1,31 @@
+resource "cloudflare_account_token" "policy-order-test" {
+  name       = "%[1]s"
+  account_id = "%[2]s"
+
+  policies = [
+    {
+      effect = "allow"
+      resources = {
+        "com.cloudflare.api.account.%[2]s" = "*"
+      }
+      permission_groups = [
+        { id = "%[3]s" },
+      ]
+    },
+    {
+      effect = "allow"
+      resources = {
+        "com.cloudflare.api.account.%[2]s" = "*"
+      }
+      permission_groups = [
+        { id = "%[4]s" },
+      ]
+    }
+  ]
+
+  condition = {
+    request_ip = {
+      in = ["127.0.0.1/32"]
+    }
+  }
+}
